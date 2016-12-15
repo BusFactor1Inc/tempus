9afba6c1f1fdf888c15afbcc19d1960a87c910d5
Burton Samograd <burton.samograd@gmail.com>
2016-12-15 00:20:15 -0700

    Added 'loadup' example.

--- README.md
+++ README.md
@@ -62,6 +62,56 @@ struct {
 The input 'data' file would be C code defining the functions and the
 array of Proceses to be fed to the scheduler.
 
+```c
+#include "fbanim.h"
+#include "fbanim.c"
+
+typedef struct {
+       unsigned int start_frame;
+       unsigned int end_frame;
+       unsigned int period;
+       unsigned int priority;
+       void* function;
+       unsigned long argument;
+} Process;
+
+void proc1(int arg) {
+  printf(".");
+}
+
+void proc2(int arg) {
+  printf(",");
+}
+
+Process procs[] = {
+  { 0, -1, 1, 0, proc1, 0 },
+  { 0, -1, 1, 0, proc2, 0 }
+};
+
+int nprocs = sizeof(procs)/sizeof(procs[0]);
+
+void loadup() {
+  for(int i = 0; i < nprocs; i++) {
+    Process *proc = &procs[i];
+    InsertProc(proc->start_frame,
+	       proc->end_frame,
+	       proc->period,
+	       proc->priority,
+	       proc->function,
+	       proc->argument);
+  }
+}
+
+int main() {
+  ListInit();
+  InitFrameBasedAnimSystem();
+
+  loadup();
+
+  RunFrameBasedAnimSystem();
+}
+```
+
 TODO
 ----
 
--- main.c
+++ main.c
@@ -1,6 +1,15 @@
 #include "fbanim.h"
 #include "fbanim.c"
 
+typedef struct {
+       unsigned int start_frame;
+       unsigned int end_frame;
+       unsigned int period;
+       unsigned int priority;
+       void* function;
+       unsigned long argument;
+} Process;
+
 void proc1(int arg) {
   printf(".");
 }
@@ -9,15 +18,30 @@ void proc2(int arg) {
   printf(",");
 }
 
+Process procs[] = {
+  { 0, -1, 1, 0, proc1, 0 },
+  { 0, -1, 1, 0, proc2, 0 }
+};
+
+int nprocs = sizeof(procs)/sizeof(procs[0]);
+
+void loadup() {
+  for(int i = 0; i < nprocs; i++) {
+    Process *proc = &procs[i];
+    InsertProc(proc->start_frame,
+	       proc->end_frame,
+	       proc->period,
+	       proc->priority,
+	       proc->function,
+	       proc->argument);
+  }
+}
+
 int main() {
   ListInit();
   InitFrameBasedAnimSystem();
-  InsertProc(0,		/* start frame	*/
-  	     -1,	/* end frame	*/
-	     1,		/* period	*/
-	     0,		/* priority	*/
-	     proc1,	/* function	*/
-	     0		/* argument	*/);
-  InsertProc(0, -1, 1, 0, proc2, 0);
+
+  loadup();
+
   RunFrameBasedAnimSystem();
 }
